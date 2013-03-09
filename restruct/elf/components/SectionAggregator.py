24b8e97e1686df7c817876800d6bd475d0bad756
youmu <tsukuyo21@gmail.com>
2013-03-09 22:32:10 +0900

    add restruct.py

--- restruct/elf/components/SectionAggregator.py
+++ restruct/elf/components/SectionAggregator.py
@@ -3,13 +3,21 @@ class SectionAggregator(object):
 
     def __init__(self):
         self.sectionList = []
+        self.nameList = []
 
     def append(self, section):
-        self.sectionList.append(section)
+        self.sectionList.append((section, section.getName()))
 
     def count(self):
         return len(self.sectionList)
 
     def dump(self):
-        for sec in self.sectionList:
-            sec.echo()
+        [sec.echo() for (sec, name) in self.sectionList]
+
+    def find(self, needle):
+        for (idx, (sec, name)) in enumerate(self.sectionList):
+            if name == needle:
+                return idx
+
+    def remove(self, name):
+        del(self.sectionList[self.find(name)])
